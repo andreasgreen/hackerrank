dcfbd7189c0ac908bc95311f62ac7d20f84c0fc5
andreas <andreas.green@gmail.com>
2018-08-23 13:12:29 +0200

    JavaRegex.

---
+++ src/main/java/javas/JavaRegex.java
@@ -0,0 +1,24 @@
+package javas;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+/**
+ * Created by andreas on 2018-08-19.
+ */
+public class JavaRegex {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    protected String pattern = "^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$";
+}
